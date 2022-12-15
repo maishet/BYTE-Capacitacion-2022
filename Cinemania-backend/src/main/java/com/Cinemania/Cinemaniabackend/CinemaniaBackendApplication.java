c04a6964020f621be9d75c795d730fe0cd6a2f3c
MOISES <moi.982555147@gmail.com>
2022-12-15 17:14:08 -0500

    create proyect

---
+++ Cinemania-backend/src/main/java/com/Cinemania/Cinemaniabackend/CinemaniaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Cinemania.Cinemaniabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaniaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaniaBackendApplication.class, args);
+	}
+
+}
---
+++ Cinemania-backend/src/test/java/com/Cinemania/Cinemaniabackend/CinemaniaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cinemania.Cinemaniabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaniaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
